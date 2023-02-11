a9c2cb89acb0fa940f7879f0323123665a684aea
P4R90 <113446863+P4R90@users.noreply.github.com>
2023-02-11 03:55:20 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// News Letter Form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  var slideIndex = 0;
+showSlides();
+
+
+// Image Slide Function
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
